82bf7e3e3dafed0299d2cce11903b2d9d66658db
Vinicius De Aquino Ferreira <viniciusaquinodev07@gmail.com>
2026-01-23 13:13:43 -0300

    feat: add TouchableOpacityProps

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,13 +1,13 @@
-import { Text, TouchableOpacity } from "react-native";
+import { Text, TouchableOpacity, type TouchableOpacityProps } from "react-native";
 import { styles } from "./styles";
 
-interface ButtonProps {
+interface ButtonProps extends TouchableOpacityProps {
   title: string;
 }
 
-export function Button({ title }: ButtonProps) {
+export function Button({ title, ...props }: ButtonProps) {
   return (
-    <TouchableOpacity style={styles.container} activeOpacity={0.5}>
+    <TouchableOpacity style={styles.container} activeOpacity={0.5} {...props}>
       <Text style={styles.title}>{title}</Text>
     </TouchableOpacity>
   );
